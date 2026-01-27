9304e50f6fdd9ea61eaa583b3ad1ed07aa460a1e
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-01-27 18:44:35 -0500

    feat: implement lab 1 exercise 3

---
+++ JP_COMP254Lab1/exercise3.py
@@ -0,0 +1,42 @@
+from typing import Iterator, Self, TypeVar
+
+from comp254 import CircularlyLinkedList, CircularNode
+
+T = TypeVar("T")
+
+
+class CloneableCircularlyLinkedList(CircularlyLinkedList[T]):
+    def clone(self) -> Self:
+        copy = type(self)()
+
+        for node in self._iter_nodes():
+            copy.add_last(node.element)
+
+        return copy
+
+
+    def _iter_nodes(self) -> Iterator[CircularNode[T]]:
+        if self.tail is None:
+            return
+
+        current = self.tail.next
+        yield current
+
+        while current is not self.tail:
+            current = current.next
+            yield current
+
+
+def main() -> None:
+    L = CloneableCircularlyLinkedList[int]()
+    L.add_first(2)
+    L.add_first(1)
+    L.add_last(3)
+    print("L:     ", L)
+
+    M = L.clone()
+    print("M copy:", M)
+
+
+if __name__ == "__main__":
+    main()
