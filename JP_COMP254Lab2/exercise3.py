fe1a43a26d0fa9cc59316ef0e581afb01341174f
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-02-10 19:09:17 -0500

    feat: implement unique functions for exercise 3

---
+++ JP_COMP254Lab2/exercise3.py
@@ -0,0 +1,24 @@
+def main() -> None:
+    ...
+
+
+def unique1(data: list[int]) -> bool:
+    n = len(data)
+    for j in range(n):
+        for k in range(j + 1, n):
+            if data[j] == data[k]:
+                return False
+    return True
+
+
+def unique2(data: list[int]) -> bool:
+    n = len(data)
+    temp = sorted(data)  # implied copy
+    for j in range(n - 1):
+        if temp[j] == temp[j + 1]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
