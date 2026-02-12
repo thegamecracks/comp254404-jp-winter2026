3febb0b5d6aa132eea38979676efa9d472800024
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-02-12 13:41:18 -0500

    feat: find optimal runtime of unique functions

--- JP_COMP254Lab2/exercise3.py
+++ JP_COMP254Lab2/exercise3.py
@@ -1,8 +1,31 @@
-from typing import Sequence
+import math
+import time
+from dataclasses import dataclass
+from typing import Callable, ParamSpec, Sequence, TypeVar
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+DURATION = 1
 
 
 def main() -> None:
-    ...
+    results = find_optimal_runtime(
+        unique1,
+        start=DURATION * 500,  # hint: it's very slow!
+        end=DURATION * 4000,
+        duration=DURATION,
+    )
+    print(results)
+
+    print()
+    results = find_optimal_runtime(
+        unique2,
+        start=DURATION * 10_000_000,
+        end=DURATION * 15_000_000,
+        duration=DURATION,
+    )
+    print(results)
 
 
 def unique1(data: Sequence[int]) -> bool:
@@ -21,6 +44,71 @@ def unique2(data: Sequence[int]) -> bool:
         if temp[j] == temp[j + 1]:
             return False
     return True
+
+
+@dataclass
+class OptimalRuntime:
+    n: int
+    duration: float
+    trials: int
+    total_elapsed: float
+
+    def __str__(self) -> str:
+        return (
+            f"Found n = {self.n} => {self.duration:.3f}s after {self.trials} trials, "
+            f"took {self.total_elapsed:.3f}s"
+        )
+
+
+def find_optimal_runtime(
+    func: Callable[[Sequence[int]], bool],
+    *,
+    start: int,
+    end: int,
+    duration: float,
+) -> OptimalRuntime:
+    max_trials = math.ceil(math.log2(end - start)) if start + 1 < end else 0
+    print(f"{start    = :,}")
+    print(f"{end      = :,}")
+    print(f"{duration = :.3f}s")
+    print(f"Finding optimal runtime for {func.__name__}, max {max_trials} trials...")
+
+    trials = 0
+    elapsed = 0.0
+    time_start = time.perf_counter()
+
+    # Below condition makes end exclusive, range is [start, end)
+    while (mid := start + (end - start) // 2) != start:
+        trials += 1
+
+        print(f"  {trials:2d}. {mid:,}", end="", flush=True)
+        data = generate_unique_data(mid)
+        elapsed = timed_call(func, data)
+        print(f" ({elapsed:.3f}s)")
+
+        if math.isclose(elapsed, duration, abs_tol=1e-3):  # 1ms tolerance
+            break
+        elif elapsed > duration:
+            end = mid
+        else:
+            start = mid
+
+    return OptimalRuntime(
+        n=mid,
+        duration=elapsed,
+        trials=trials,
+        total_elapsed=time.perf_counter() - time_start,
+    )
+
+
+def generate_unique_data(n: int) -> Sequence[int]:
+    return range(n)  # no memory overhead
+
+
+def timed_call(func: Callable[P, T], *args: P.args, **kwargs: P.kwargs) -> float:
+    start = time.perf_counter()
+    func(*args, **kwargs)
+    return time.perf_counter() - start
 
 
 if __name__ == "__main__":
