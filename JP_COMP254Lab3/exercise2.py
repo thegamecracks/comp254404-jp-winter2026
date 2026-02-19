d6dbce3b77e4c2253108b0996aa9cde045530df0
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-02-19 15:00:20 -0500

    feat: implement lab 3 exercise 2

--- JP_COMP254Lab3/exercise2.py
+++ JP_COMP254Lab3/exercise2.py
@@ -1,5 +1,22 @@
 def main() -> None:
-    pass
+    s = input("Enter a line: ")
+    while s:
+        print(f"Is that string a palindrome? {'Yes!' if is_palindrome(s) else 'No.'}")
+        s = input(": ")
+
+
+def is_palindrome(s: str, start: int = -1, end: int = -1) -> bool:
+    if len(s) < 1:
+        return True
+    elif start < 0 or end < 0:
+        return is_palindrome(s, 0, len(s) - 1)
+
+    if s[start] != s[end]:
+        return False
+    elif start >= end:  # middle
+        return True
+
+    return is_palindrome(s, start + 1, end - 1)
 
 
 if __name__ == "__main__":
