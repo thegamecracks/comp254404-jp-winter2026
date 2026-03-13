2e37906faea13260983c9ebcb011349b38986a28
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-03-13 10:06:48 -0400

    feat: add implementation for exercise 1

--- JP_COMP254Lab4/app/src/main/java/exercise1/Exercise1.java
+++ JP_COMP254Lab4/app/src/main/java/exercise1/Exercise1.java
@@ -1,8 +1,23 @@
 package exercise1;
 
+import java.util.Iterator;
+
+import lists.Position;
+
 public class Exercise1 {
     public static void main(String[] args) throws Exception {
         JPLinkedPositionalList<String> list = new JPLinkedPositionalList<>();
+
+        list.addLast("a");
+        list.addLast("b");
+        list.addLast("c");
+        list.addLast("d");
         System.out.println(list);
+
+        Iterator<Position<String>> it = list.positions().iterator();
+        while (it.hasNext()) {
+            Position<String> p = it.next();
+            System.out.printf("%s is at index: %d%n", p.getElement(), list.indexOf(p));
+        }
     }
 }
