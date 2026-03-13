4b9b2d9c2e36047459ac651f76ecf914dd16a9c1
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-03-13 10:06:32 -0400

    fix: reimplement indexOf() with Position parameter

--- JP_COMP254Lab4/app/src/main/java/exercise1/JPPositionalList.java
+++ JP_COMP254Lab4/app/src/main/java/exercise1/JPPositionalList.java
@@ -2,16 +2,17 @@ package exercise1;
 
 import java.util.Iterator;
 
+import lists.Position;
 import lists.PositionalList;
 
 public interface JPPositionalList<E> extends PositionalList<E> {
-    default int indexOf(E e) {
+    default int indexOf(Position<E> p) {
         int i = 0;
-        Iterator<E> it = iterator();
+        Iterator<Position<E>> it = positions().iterator();
         while (it.hasNext()) {
-            E current = it.next();
+            Position<E> current = it.next();
 
-            if (current.equals(e))
+            if (current.equals(p))
                 return i;
 
             i++;
