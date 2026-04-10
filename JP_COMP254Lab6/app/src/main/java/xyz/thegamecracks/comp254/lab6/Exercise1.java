1d1c43c3e8eb5c0fa72953e9a588f798d27d703a
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-04-10 13:30:31 -0400

    feat: setup map benchmark for exercise 1

--- JP_COMP254Lab6/app/src/main/java/xyz/thegamecracks/comp254/lab6/Exercise1.java
+++ JP_COMP254Lab6/app/src/main/java/xyz/thegamecracks/comp254/lab6/Exercise1.java
@@ -1,22 +1,32 @@
 package xyz.thegamecracks.comp254.lab6;
 
+import java.util.Random;
+
 import xyz.thegamecracks.comp254.lab6.maps.AbstractHashMap;
 import xyz.thegamecracks.comp254.lab6.maps.ChainHashMap;
 
 public class Exercise1 {
+    private static final Random RAND = new Random();
+    private static final int CAPACITY = 10;
+    private static final int PRIME = 109345121;
+    private static final int N_ENTRIES = 1_000_000;
+
     public static void main(String[] args) {
-        int capacity = 10;
-        int prime = 109345121;
-        for (double loadFactor = 0.1; loadFactor < 1.0; loadFactor += 0.1) {
-            AbstractHashMap<Integer, String> map = new ChainHashMap<>(capacity, prime, loadFactor);
-            benchmarkMap(map);
+        for (double i = 1; i < 10; i += 1) {
+            benchmarkMap(i * 0.1);
         }
     }
 
-    public static void benchmarkMap(AbstractHashMap<Integer, String> map) {
-        // TODO: use random values and measure performance
-        for (int key = 0; key < 100; key++) {
-            map.put(key, String.format("Hello world #%d!", key));
+    public static void benchmarkMap(double loadFactor) {
+        System.out.printf("Benchmarking ChainHashMap with load factor %.1f...\n", loadFactor);
+        AbstractHashMap<Integer, Integer> map = new ChainHashMap<>(CAPACITY, PRIME, loadFactor);
+
+        long start = System.currentTimeMillis();
+        for (int value = 0; value < N_ENTRIES; value++) {
+            map.put(RAND.nextInt(), value);
         }
+
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.printf("  Completed in %dms\n", elapsed);
     }
 }
