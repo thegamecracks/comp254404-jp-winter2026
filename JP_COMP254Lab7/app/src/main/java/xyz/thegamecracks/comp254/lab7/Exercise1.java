a7ea0e4acccc0605b53c7a705f07d55ca5b27bbc
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-04-16 16:17:16 -0400

    feat!: replace lab 7 java project with python scripts

--- JP_COMP254Lab7/app/src/main/java/xyz/thegamecracks/comp254/lab7/Exercise1.java
+++
@@ -1,7 +0,0 @@
-package xyz.thegamecracks.comp254.lab7;
-
-public class Exercise1 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ JP_COMP254Lab7/exercise2.py
@@ -0,0 +1,7 @@
+# NOTE: this script does not require installing the previous comp254 package.
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
